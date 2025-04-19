cf10cc1f3fe86d6148643a832ce03519eae5885d
Vincend <vincendwa@gmail.com>
2025-04-19 16:46:20 +0300

    init

---
+++ autotests/daft.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    name = browser.find_element(By.CSS_SELECTOR, 'Enter first name')
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
